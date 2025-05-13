1aa7203c9476ae31fd07ce067e8fcf89c5c7c1bc
werule2314 <igorek7894@gmail.com>
2025-05-14 02:43:25 +0300

    test: добавлен файл test_env.py для проверки загрузки .env v2

--- test_env.py
+++ test_env.py
@@ -3,4 +3,10 @@ import os
 
 load_dotenv()
 
-print(os.getenv("DB_NAME"))
+print("Проверка переменных окружения:")
+print("DB_HOST =", os.getenv("DB_HOST"))
+print("DB_PORT =", os.getenv("DB_PORT"))
+print("DB_NAME =", os.getenv("DB_NAME"))
+print("DB_USER =", os.getenv("DB_USER"))
+print("DB_PASSWORD =", os.getenv("DB_PASSWORD"))
+print("SECRET_KEY =", os.getenv("SECRET_KEY"))
