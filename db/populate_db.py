33e1a2a6a8ae2c7bc0d459a811d98a9a7e8dd442
werule2314 <igorek7894@gmail.com>
2025-05-14 01:56:31 +0300

    fix: исправлен импорт db для запуска populate_db как скрипта

--- db/populate_db.py
+++ db/populate_db.py
@@ -3,6 +3,10 @@ from sqlalchemy.orm import sessionmaker
 from utils.security import encrypt_text
 from dotenv import load_dotenv
 import random
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 load_dotenv()
 
